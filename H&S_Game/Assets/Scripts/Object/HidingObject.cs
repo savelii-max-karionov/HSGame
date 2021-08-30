33b6ec3f29dfb5aad7761007234ca74bfc3c986d
savelii-max-karionov <savelii.karionov@gmail.com>
2021-08-30 15:02:51 -0700

    Hiding Objects fix (prototype)

--- H&S_Game/Assets/Scripts/Object/HidingObject.cs
+++ H&S_Game/Assets/Scripts/Object/HidingObject.cs
@@ -6,26 +6,22 @@ public class HidingObject : MonoBehaviour
 {
     [SerializeField] private BoxCollider2D hideCollider;
     [SerializeField] private GameObject playerVisual;
+    [SerializeField] private Animator animator;
+    private bool open;
     // Start is called before the first frame update
     void Start()
     {
-        
+        open = false;
+    }
+
+    public void Open()
+    {
+        open = !open;
+        animator.SetBool("Open", open);
     }
 
     // Update is called once per frame
     void Update()
     {
     }
-    private void OnMouseOver()
-    {
-        //If your mouse hovers over the GameObject with the script attached, output this message
-        //Debug.Log("Mouse is over GameObject.");
-    }
-    private void OnMouseDown()
-    {
-        Debug.Log("Clicked on the Hiding Object");
-        playerVisual.GetComponent<SpriteRenderer>().enabled = false;
-    }
-
-
 }
